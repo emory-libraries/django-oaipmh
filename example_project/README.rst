e76b81962177089391f7847ba838e6b02f182459
Bertrand Bordage <bordage.bertrand@gmail.com>
2013-04-16 01:08:01 +0200

    Adds an example project.

---
+++ example_project/README.rst
@@ -0,0 +1,14 @@
+Example project
+===============
+
+This example project uses `django.contrib.flatpages`.
+
+First, copy or link django_oaipmh in that directory then just run
+``python manage.py runserver`` and open http://localhost:8000/ in your browser.
+
+Login:
+
+======== =====
+Username admin
+Password admin
+======== =====
---
+++ example_project/example_project/views.py
@@ -0,0 +1,20 @@
+# coding: utf-8
+
+from __future__ import unicode_literals
+from datetime import datetime
+from django.contrib.flatpages.models import FlatPage
+from django_oaipmh import OAIProvider
+
+
+class ExampleOAIProvider(OAIProvider):
+    def items(self):
+        return FlatPage.objects.filter()
+
+    def last_modified(self, obj):
+        return datetime.now()
+
+    def oai_identifier(self, obj):
+        return 'example_project'
+
+    def sets(self, obj):
+        return []
